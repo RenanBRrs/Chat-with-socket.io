c3c82ec43ca5b00bf0b733c35a8e295569a4b915
RenanBRrs <renandossantosbrito@hotmail.com>
2021-09-28 21:53:41 -0300

    socket OK

--- back-end/App.js
+++ back-end/App.js
@@ -3,26 +3,39 @@ const socket = require('socket.io');
 const cors = require('cors');
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header(
     'Access-Control-Allow-Headers',
-    'X-PINGOTHER,Content-Type, Authorization',
+    'X-PINGOTHER, Content-Type, Authorization',
   );
   app.use(cors());
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Olas mundo...');
+app.get('/', function (req, res) {
+  res.send('Bem vindo!');
 });
+
 const server = app.listen(8080, () => {
-  console.log('Rodando...');
+  console.log('Servidor iniciado na porta 8080: http://localhost:8080');
 });
 
 io = socket(server, { cors: { origin: '*' } });
 
-io.on('Connection', (socket) => {
+io.on('connection', (socket) => {
   console.log(socket.id);
+
+  socket.on('sala_conectar', (dados) => {
+    console.log('Sala selecionada: ' + dados);
+    socket.join(dados);
+  });
+
+  socket.on('enviar_mensagem', (dados) => {
+    console.log(dados);
+    socket.to(dados.sala).emit('receber_mensagem', dados.conteudo);
+  });
 });
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+let socket;
 
 function App() {
+  const ENDPOINT = 'http://localhost:8080/';
+
+  const [logado, setLogado] = useState(false);
+  const [nome, setNome] = useState('');
+  const [sala, setSala] = useState('');
+
+  const [mensagem, setMensagem] = useState('');
+  const [listaMensagem, setListaMensagem] = useState([]);
+
+  useEffect(() => {
+    socket = socketIOClient(ENDPOINT);
+  }, []);
+  useEffect(() => {
+    socket.on('receber_mensagem', (dados) => {
+      setListaMensagem([...listaMensagem, dados]);
+    });
+  });
+  const conectarSala = () => {
+    console.log('Voce acessou a sala ' + sala + ' com o usuario ' + nome);
+    setLogado(true);
+    socket.emit('sala_conectar', sala);
+  };
+
+  const enviarMensagem = async () => {
+    console.log('Mensagem ' + mensagem);
+    const conteudoMensagem = {
+      sala,
+      conteudo: {
+        nome,
+        mensagem,
+      },
+    };
+    console.log(conteudoMensagem);
+
+    await socket.emit('enviar_mensagem', conteudoMensagem);
+    setListaMensagem([...listaMensagem, conteudoMensagem.conteudo]);
+    setMensagem('');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chat</h1>
+      {!logado ? (
+        <>
+          <label>Nome </label>
+          <input
+            type='text'
+            placeholder='Nome'
+            name='nome'
+            value={nome}
+            onChange={(texto) => {
+              setNome(texto.target.value);
+            }}
+          />
+          <br />
+          <br />
+          <label>Sala </label>
+          {/* <input
+            type='text'
+            placeholder='Sala'
+            name='sala'
+            value={sala}
+            onChange={(text) => {
+              setSala(text.target.value);
+            }}
+          /> */}
+
+          <select
+            name='sala'
+            value={sala}
+            onChange={(texto) => setSala(texto.target.value)}>
+            <option value=''>Selecione</option>
+            <option value='1'>Node</option>
+            <option value='2'>React</option>
+            <option value='3'>React Native</option>
+            <option value='4'>PHP</option>
+          </select>
+          <br />
+          <br />
+          <button onClick={conectarSala}>Acessar</button>
+        </>
+      ) : (
+        <>
+          {listaMensagem.map((msg, key) => {
+            return (
+              <div key={key}>
+                {msg.nome}: {msg.mensagem}
+              </div>
+            );
+          })}
+          <input
+            type='text'
+            name='mensagem'
+            placeholder='Mensagem...'
+            value={mensagem}
+            onChange={(texto) => {
+              setMensagem(texto.target.value);
+            }}
+          />
+          <button onClick={enviarMensagem}>Enviar</button>
+        </>
+      )}
     </div>
   );
 }
